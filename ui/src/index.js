2ea078b9f36492cb39cf561bbdc8e1da41d55afa
Bleart Nuredini <nuredini.bleart@gmail.com>
2024-10-09 10:45:16 +0200

    feat: add support for different time intervals

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,8 +1,7 @@
 import { randomText } from "./words.js";
 
-const tmpWordCnt = document.getElementById("word-cnt");
 const gameTextElem = document.getElementById("game-text");
-const text = randomText(120);
+const text = randomText(200);
 
 text.split("").forEach((ch) => {
   const letterElem = document.createElement("letter");
@@ -18,10 +17,16 @@ let idx = 0; // index of the next expected character
 let wordCnt = 0;
 let gameActive = false;
 let gameFinished = false;
+const wordCntElem = document.getElementById("word-cnt");
 
 document.addEventListener("keydown", (e) => {
-  if (e.key == "Backspace" || e.keyCode == 8) {
-    e.preventDefault(); // Firefox has backspace mapped to the 'Back' function
+  if (
+    e.key == "Backspace" ||
+    e.keyCode == 8 || // Firefox has backspace mapped to the 'Back' function
+    e.key == " " ||
+    e.keyCode == 32
+  ) {
+    e.preventDefault();
   }
 
   if (idx == 0) {
@@ -36,15 +41,15 @@ document.addEventListener("keydown", (e) => {
   if (e.key == text[idx]) {
     gameTextElem.children[idx].classList.add("active");
 
-    if (idx + 1 < text.length) {
+    if (idx < text.length - 1) {
       gameTextElem.children[idx + 1].appendChild(cursorElem);
     } else {
       gameTextElem.children[idx].removeChild(cursorElem);
     }
 
-    if (idx + 1 >= gameTextElem.children.length) {
+    if (idx >= gameTextElem.children.length - 1) {
       wordCnt++;
-      tmpWordCnt.innerText = wordCnt;
+      wordCntElem.innerText = wordCnt;
       idx++;
       finishGame();
       return;
@@ -52,56 +57,111 @@ document.addEventListener("keydown", (e) => {
 
     if (text[idx + 1] == " " && isCurrWordValid(idx)) {
       wordCnt++;
-      tmpWordCnt.innerText = wordCnt;
+      wordCntElem.innerText = wordCnt;
     }
 
     idx++;
   } else if (e.key == "Backspace" || e.keyCode == 8) {
-    if (idx > 0) {
-      idx--;
+    if (idx == 0) {
+      return;
+    }
 
-      // remove cursor if this isn't the last character
-      if (idx < gameTextElem.children.length - 1) {
-        gameTextElem.children[idx + 1].removeChild(cursorElem);
-      }
+    idx--;
 
-      if (text[idx + 1] == " " && isCurrWordValid(idx)) {
-        wordCnt--;
-        console.log(`id: ${idx} text[idx+1]: ${text[idx + 1]}`);
-        tmpWordCnt.innerText = wordCnt;
-      }
+    // remove cursor if this isn't the last character
+    if (idx < gameTextElem.children.length - 1) {
+      gameTextElem.children[idx + 1].removeChild(cursorElem);
+    }
 
-      gameTextElem.children[idx].appendChild(cursorElem);
-      gameTextElem.children[idx].classList.remove("active");
+    if (text[idx + 1] == " " && isCurrWordValid(idx)) {
+      wordCnt--;
+      console.log(`id: ${idx} text[idx+1]: ${text[idx + 1]}`);
+      wordCntElem.innerText = wordCnt;
+    }
+
+    gameTextElem.children[idx].appendChild(cursorElem);
+    gameTextElem.children[idx].classList.remove("active");
+
+    if (text[idx] == " " || text[idx] == 32) {
+      gameTextElem.children[idx].classList.remove("wrong-whitespace");
+    } else {
       gameTextElem.children[idx].classList.remove("wrong");
     }
   } else {
-    gameTextElem.children[idx].classList.add("wrong");
+      // gameTextElem.children[idx].classList.add("wrong-whitespace");
+    console.log(`e.key: ${e.key}`);
+    console.log(`e.keyCode: ${e.keyCode}`);
+    if (text[idx] == " " || text[idx] == 32) {
+      gameTextElem.children[idx].classList.add("wrong-whitespace");
+    } else {
+      gameTextElem.children[idx].classList.add("wrong");
+    }
+
     gameTextElem.children[idx + 1].appendChild(cursorElem);
     idx++;
   }
 });
 
-const initialTime = 0.5 * 60 * 1000;
+let initialTime = +document
+  .getElementById("active-time-setter")
+  .getAttribute("value");
 let time = initialTime;
-const timerElem = document.getElementById("timer");
-timerElem.innerHTML = toMinsSecs(time);
+let currTimerInterval = setTimer();
 
-const timerInterval = setInterval(() => {
-  if (time <= 0 || gameFinished || !gameActive) {
-    return;
-  }
+document.querySelectorAll(".time-setter").forEach((elem) => {
+  elem.addEventListener("click", (e) => {
+    document.getElementById("active-time-setter").removeAttribute("id");
+    e.target.setAttribute("id", "active-time-setter");
 
-  time -= 1000;
-  timerElem.innerHTML = toMinsSecs(time);
+    const timeSetterValue = +e.target.getAttribute("value");
+    initialTime = timeSetterValue;
+    time = initialTime;
+    clearInterval(currTimerInterval);
+    currTimerInterval = setTimer();
 
-  if (time <= 0) {
-    gameFinished = true;
-    finishGame();
-    return;
-  }
-}, 1000);
+    // TODO: you need to restart the game here. Adding a restart option will require modifying the
+    // current code structure by quite a bit. An `init` needs to be introduced that resets the
+    // game text, cursor position, and other statistics/values about the current game session.
+    // This is a good opportunity to introduce purer functions and to categorize some of the
+    // responsbilities more sensibly -- and there's a lot of state that's being shared between
+    // functions, so that needs to be addressed to.
+  });
+});
 
+function setTimer() {
+  const timerElem = document.getElementById("timer");
+  timerElem.innerHTML = toMinsSecs(initialTime);
+
+  const timerInterval = setInterval(
+    () => {
+      if (time <= 0 || gameFinished || !gameActive) {
+        return;
+      }
+
+      time -= 1000;
+      timerElem.innerHTML = toMinsSecs(time);
+
+      if (time <= 0) {
+        gameFinished = true;
+        finishGame();
+
+        return;
+      }
+    },
+    1000, // every one second
+  );
+
+  return timerInterval;
+}
+
+/**
+ * Given a certain amount of milliseconds, this convert those milliseconds to
+ * the minutes:seconds format, where the seconds bit is repended with a zero
+ * (i.e., a "0" character) if the number of seconds is single digit number.
+ *
+ * @param {numer} timeInMillis a number in millis
+ * @returns {string}
+ */
 function toMinsSecs(timeInMillis) {
   const mins = Math.floor(timeInMillis / (60 * 1000));
   const secs = (timeInMillis - mins * 60 * 1000) / 1000;
@@ -114,7 +174,7 @@ function finishGame() {
   gameFinished = true;
   gameActive = false;
   document.getElementById("game-finished").classList.remove("hidden");
-  clearInterval(timerInterval);
+  clearInterval(currTimerInterval);
 
   document.getElementById("speed").innerHTML =
     "Speed: " + toWpm(wordCnt, initialTime - time) + " WPM";
@@ -134,7 +194,7 @@ function toWpm(wordCnt, timeInMillis) {
  * valid up until this letter.
  *
  * @param {number} lastIdx
- * @param {boolean}
+ * @returns {boolean}
  */
 function isCurrWordValid(lastIdx) {
   if (lastIdx < 0 || lastIdx >= gameTextElem.children.length) {
